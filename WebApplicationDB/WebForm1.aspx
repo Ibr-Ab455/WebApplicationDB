19755bbbd9bf45227ce62706dccbd47beae3a6fb
Ibrahim-ali <Khaliilali455@gmail.com>
2023-09-11 09:39:02 +0200

    Add project files.

---
+++ WebApplicationDB/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplicationDB.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <asp:Button ID="ButtonSearchLastName" runat="server" Text="søk" OnClick="ButtonSearchLastName_Click" />
+          <br />
+        <asp:TextBox ID="TextBoxLastName" runat="server"></asp:TextBox>
+         
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+         
+                
+           
+       
+    </form>
+</body>
+</html>
---
+++ WebApplicationDB/WebForm1.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationDB
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+           DataTable dt =new DataTable();
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * from person", conn);
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+
+                reader.Close();
+                conn.Close();
+            }
+        }
+
+        protected void ButtonSearchLastName_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlParameter param;
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * from person where etternavn=@lastName", conn);
+                cmd.CommandType = CommandType.Text;
+
+                param=new SqlParameter("@lastName",SqlDbType.NChar);
+                param.Value=TextBoxLastName.Text;
+                cmd.Parameters.Add(param);
+
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+
+                reader.Close();
+                conn.Close();
+            }
+        }
+    }
+}
